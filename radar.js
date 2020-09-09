3ce4de2cd2ec1eec9a981f833e613dc07838bbe9
nikeiubel <nfi2103@columbia.edu>
2020-09-09 12:19:57 +0200

    fixing JS syntax typo

--- radar.js
+++ radar.js
@@ -264,7 +264,7 @@ function radar_visualization(config) {
     if (ring == 1 || ring == 2) {
 	dy = dy + 48*ring + segmented[quadrant][ring-1].length * 16;
        // hardcoding fix for demo, this will have to be fixed eventually
-       if (ring == 2 & quadrant = 3) {
+       if (ring == 2 && quadrant == 3) {
            dy = dy + 48;
        }	
     }
